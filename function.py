616e7e12b760bab40d086c2bd81c2ccf4afde031
Lee Jeong Min <ekdmsdlfma@gmail.com>
2021-05-23 18:54:47 +0900

    test

---
+++ function.py
@@ -0,0 +1,448 @@
+import cv2
+from time import sleep
+import time
+import glob
+from mecha_drive import *
+
+def grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+def canny(img, low_threshold, high_threshold):
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def median_blur(img, kernel_size):
+    return cv2.medianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices, color3=(255,255,255), color1=255):
+	
+    mask = np.zeros_like(img)
+    
+    if len(img.shape) > 2:
+        color = color3
+    else:
+        color = color1
+        
+    cv2.fillPoly(mask, vertices, color)
+    
+    ROI_image = cv2.bitwise_and(img, mask)
+    return ROI_image
+
+def draw_crossLines(image):
+    height, width = image.shape[:2]
+
+    # draw horizontal lines
+    image = cv2.line(image, (0, int(height/4)*1), (width , int(height/4)*1), (180, 180, 180), 1)
+    image = cv2.line(image, (0, int(height/4)*2), (width , int(height/4)*2), (180, 180, 180), 1)
+    image = cv2.line(image, (0, int(height/4)*3), (width , int(height/4)*3), (180, 180, 180), 1)
+
+    # draw vertical lines
+    image = cv2.line(image, (int(width/8)*1, 0), (int(width/8)*1, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*2, 0), (int(width/8)*2, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*3, 0), (int(width/8)*3, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*4, 0), (int(width/8)*4, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*5, 0), (int(width/8)*5, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*6, 0), (int(width/8)*6, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*7, 0), (int(width/8)*7, int(height)), (180, 180, 180), 1)
+
+    return image
+
+def getVerticalDistance(image, v):
+    VD = 0
+    height, width = image.shape[:2]
+
+    # find vertical distance from bottom
+    for y in range(height+1):
+        b = image.item(height-1-y, v, 0)  
+        g = image.item(height-1-y, v, 1)  
+        r = image.item(height-1-y, v, 2)
+        if b == 255 and g == 255 and r == 255:
+            VD = y
+            break
+    return VD
+        
+def getHorizontalDistance(image, h):
+    height, width = image.shape[:2]
+    center = int(width/2)
+    HLD = center
+    HRD = center
+
+    # find Horizontal Left Distance from center line
+    for x in range(center+1):
+        b = image.item(h, center-x, 0)  
+        g = image.item(h, center-x, 1)  
+        r = image.item(h, center-x, 2)
+
+        if b == 255 and g == 255 and r == 255:
+            HLD = x
+            break
+
+    # find Horizontal Right Distance from center line
+    for x in range(center):
+        b = image.item(h, center+x, 0)  
+        g = image.item(h, center+x, 1)  
+        r = image.item(h, center+x, 2)
+
+        if b == 255 and g == 255 and r == 255:
+            HRD = x
+            break
+
+    return HLD, HRD
+
+def getContactPoints(image):
+    height, width = image.shape[:2]
+    
+    # get vertical lengths
+    V1D = getVerticalDistance(image, int(width/8)*1)
+    V2D = getVerticalDistance(image, int(width/8)*2)
+    V3D = getVerticalDistance(image, int(width/8)*3)
+    V4D = getVerticalDistance(image, int(width/8)*4)
+    V5D = getVerticalDistance(image, int(width/8)*5)
+    V6D = getVerticalDistance(image, int(width/8)*6)
+    V7D = getVerticalDistance(image, int(width/8)*7)
+
+    # get horizontal lenghts
+    H1LD, H1RD = getHorizontalDistance(image, int(height/4)*1)
+    H2LD, H2RD = getHorizontalDistance(image, int(height/4)*2)
+    H3LD, H3RD = getHorizontalDistance(image, int(height/4)*3)
+
+    return {'V1D':V1D, 'V2D':V2D, 'V3D':V3D, 'V4D':V4D, 'V5D':V5D, 'V6D':V6D, 'V7D':V7D, \
+    'H1LD':H1LD, 'H1RD':H1RD, 'H2LD':H2LD, 'H2RD':H2RD, 'H3LD':H3LD, 'H3RD':H3RD}
+
+def drawContactPoints(image, points):        
+    height, width = image.shape[:2]        
+    center = int(width/2)
+
+    # draw vertical points
+    image = cv2.circle(image, (center-points['H1LD'], int(height/4)*1), 1, (255, 0, 0), 2)
+    image = cv2.circle(image, (center+points['H1RD']-1, int(height/4)*1), 1, (0, 0, 255), 2)
+    image = cv2.circle(image, (center-points['H2LD'], int(height/4)*2), 1, (255, 0, 0), 2)
+    image = cv2.circle(image, (center+points['H2RD']-1, int(height/4)*2), 1, (0, 0, 255), 2)
+    image = cv2.circle(image, (center-points['H3LD'], int(height/4)*3), 1, (255, 0, 0), 2)
+    image = cv2.circle(image, (center+points['H3RD']-1, int(height/4)*3), 1, (0, 0, 255), 2)
+
+    # draw horizontal points
+    image = cv2.circle(image, (int(width/8)*1, height-points['V1D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*2, height-points['V2D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*3, height-points['V3D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*4, height-points['V4D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*5, height-points['V5D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*6, height-points['V6D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*7, height-points['V7D']), 1, (0, 255, 255), 2)
+
+    return image
+
+
+
+# functions for the algorithm   
+def getLean(line):
+    
+
+    if line[0]-line[2] == 0:
+        return 10000
+    print(line[1]-line[3],line[0]-line[2])
+    #lean = float(line[1]-line[3])/(line[0]-line[2])
+   # print(lean)
+    return lean
+
+def getIntercept(line):
+    x = line[0]
+    y = line[1]
+    a = getLean(line)
+    b = y-(a*x)
+    return (120-b)/a
+
+def draw_lines(img, lines, color=[0, 0, 255], thickness=2):
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap): 
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+
+    return line_img, lines
+    
+def weighted_img(img, initial_img, alpha=1, beta=1., gamma=0.):
+    return cv2.addWeighted(initial_img, alpha, img, beta, gamma)
+
+def getAlignedWheelAngle(command, alignAlgle):
+    steeringAngle = int(command[2:5]) + alignAlgle
+    return command[:2] + str(steeringAngle) + 'E'
+
+def getLane(lines):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+def canny(img, low_threshold, high_threshold):
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices, color3=(255,255,255), color1=255):
+	
+    mask = np.zeros_like(img)
+    
+    if len(img.shape) > 2:
+        color = color3
+    else:
+        color = color1ROI_image
+    cv2.fillPoly(mask, vertices, color)
+    
+    ROI_image = cv2.bitwise_and(img, mask)
+    return ROI_image
+
+def draw_crossLines(image):
+    height, width = image.shape[:2]
+
+    # draw horizontal lines
+    image = cv2.line(image, (0, int(height/4)*1), (width , int(height/4)*1), (180, 180, 180), 1)
+    image = cv2.line(image, (0, int(height/4)*2), (width , int(height/4)*2), (180, 180, 180), 1)
+    image = cv2.line(image, (0, int(height/4)*3), (width , int(height/4)*3), (180, 180, 180), 1)
+
+    # draw vertical lines
+    image = cv2.line(image, (int(width/8)*1, 0), (int(width/8)*1, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*2, 0), (int(width/8)*2, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*3, 0), (int(width/8)*3, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*4, 0), (int(width/8)*4, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*5, 0), (int(width/8)*5, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*6, 0), (int(width/8)*6, int(height)), (180, 180, 180), 1)
+    image = cv2.line(image, (int(width/8)*7, 0), (int(width/8)*7, int(height)), (180, 180, 180), 1)
+
+    return image
+
+def getVerticalDistance(image, v):
+    VD = 0
+    height, width = image.shape[:2]
+
+    # find vertical distance from bottom
+    for y in range(height+1):
+        b = image.item(height-1-y, v, 0)  
+        g = image.item(height-1-y, v, 1)  
+        r = image.item(height-1-y, v, 2)
+        if b == 255 and g == 255 and r == 255:
+            VD = y
+            break
+    return VD
+        
+def getHorizontalDistance(image, h):
+    height, width = image.shape[:2]
+    center = int(width/2)
+    HLD = center
+    HRD = center
+
+    # find Horizontal Left Distance from center line
+    for x in range(center+1):
+        b = image.item(h, center-x, 0)  
+        g = image.item(h, center-x, 1)  
+        r = image.item(h, center-x, 2)
+
+        if b == 255 and g == 255 and r == 255:
+            HLD = x
+            break
+
+    # find Horizontal Right Distance from center line
+    for x in range(center):
+        b = image.item(h, center+x, 0)  
+        g = image.item(h, center+x, 1)  
+        r = image.item(h, center+x, 2)
+
+        if b == 255 and g == 255 and r == 255:
+            HRD = x
+            break
+
+    return HLD, HRD
+
+def getContactPoints(image):
+    height, width = image.shape[:2]
+    
+    # get vertical lengths
+    V1D = getVerticalDistance(image, int(width/8)*1)
+    V2D = getVerticalDistance(image, int(width/8)*2)
+    V3D = getVerticalDistance(image, int(width/8)*3)
+    V4D = getVerticalDistance(image, int(width/8)*4)
+    V5D = getVerticalDistance(image, int(width/8)*5)
+    V6D = getVerticalDistance(image, int(width/8)*6)
+    V7D = getVerticalDistance(image, int(width/8)*7)
+
+    # get horizontal lenghts
+    H1LD, H1RD = getHorizontalDistance(image, int(height/4)*1)
+    H2LD, H2RD = getHorizontalDistance(image, int(height/4)*2)
+    H3LD, H3RD = getHorizontalDistance(image, int(height/4)*3)
+
+    return {'V1D':V1D, 'V2D':V2D, 'V3D':V3D, 'V4D':V4D, 'V5D':V5D, 'V6D':V6D, 'V7D':V7D, \
+    'H1LD':H1LD, 'H1RD':H1RD, 'H2LD':H2LD, 'H2RD':H2RD, 'H3LD':H3LD, 'H3RD':H3RD}
+
+def drawContactPoints(image, points):        
+    height, width = image.shape[:2]        
+    center = int(width/2)
+
+    # draw vertical points
+    image = cv2.circle(image, (center-points['H1LD'], int(height/4)*1), 1, (255, 0, 0), 2)
+    image = cv2.circle(image, (center+points['H1RD']-1, int(height/4)*1), 1, (0, 0, 255), 2)
+    image = cv2.circle(image, (center-points['H2LD'], int(height/4)*2), 1, (255, 0, 0), 2)
+    image = cv2.circle(image, (center+points['H2RD']-1, int(height/4)*2), 1, (0, 0, 255), 2)
+    image = cv2.circle(image, (center-points['H3LD'], int(height/4)*3), 1, (255, 0, 0), 2)
+    image = cv2.circle(image, (center+points['H3RD']-1, int(height/4)*3), 1, (0, 0, 255), 2)
+
+    # draw horizontal points
+    image = cv2.circle(image, (int(width/8)*1, height-points['V1D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*2, height-points['V2D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*3, height-points['V3D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*4, height-points['V4D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*5, height-points['V5D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*6, height-points['V6D']), 1, (0, 255, 255), 2)
+    image = cv2.circle(image, (int(width/8)*7, height-points['V7D']), 1, (0, 255, 255), 2)
+
+    return image
+
+
+
+# functions for the algorithm
+def getLean(line):
+    if line[0]-line[2] == 0:
+        return 10000
+    return (float(line[1]-line[3]))/float((line[0]-line[2]))
+
+def getIntercept(line):
+    x = line[0]
+    y = line[1]
+    a = getLean(line)
+    b = y-(a*x)
+    return (120-b)/a
+
+def draw_lines(img, lines, color=[0, 0, 255], thickness=2):
+    try:
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+    except:
+        pass
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap): 
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+
+    return line_img, lines
+    
+def weighted_img(img, initial_img, alpha=1, beta=1., gamma=0.):
+    return cv2.addWeighted(initial_img, alpha, img, beta, gamma)
+
+def getAlignedWheelAngle(command, alignAlgle):
+    steeringAngle = int(command[2:5]) + alignAlgle
+    return command[:2] + str(steeringAngle) + 'E'
+
+def getLane(lines):
+    leftLanes = []
+    rightLanes = []
+    endLanes = []
+    rightLane = []
+    leftLane = []
+    try:
+        for line in lines.tolist():
+            
+            line = line[0]
+
+            a = getLean(line)
+            #print(a)
+            if a == 10000:
+                pass
+            elif a < -0.1 and line[2] < 200: # left lane
+                leftLanes.append(line)
+            elif a < 0.1 and a > -0.1: # end lane
+                endLanes.append(line)
+            # elif line[0] > 120:
+            elif a > 0.1:
+                rightLanes.append(line)
+
+        for right in rightLanes:
+            if len(rightLane) == 0:
+                rightLane = right
+            if getIntercept(right) < getIntercept(rightLane):
+                rightLane = right
+        for left in leftLanes:
+            if len(leftLane) == 0:
+                leftLane = left
+            if getIntercept(left) > getIntercept(leftLane):
+                leftLane = left
+    except:
+        pass
+        
+    return leftLane, rightLane, endLanes
+
+def getMidPositionOfX(lines, width):
+    sum = 0
+    for line in lines:
+        sum += (line[0] + line[2])/2
+    sum /= len(lines)
+    if sum < width/2:
+        return False # means left
+    else: return True # means right
+
+def getCenterPoint(leftLane, rightLane):
+    left_a = getLean(leftLane)
+    left_b = leftLane[3] - left_a*leftLane[2]
+    right_a = getLean(rightLane)
+    right_b = rightLane[1] - right_a*rightLane[0]
+
+    left_x = (30-left_b)/left_a
+    right_x = (30-right_b)/right_a
+
+    return left_x, right_x
+    leftLanes = []
+    rightLanes = []
+    endLanes = []
+    rightLane = []
+    leftLane = []
+    
+    try:
+        for line in lines.tolist():
+            line = line[0]
+            a = getLean(line)
+            if a == 10000:
+                pass
+            elif a < -0.2 and line[2] < 200: # left lane
+                leftLanes.append(line)
+            elif a < 0.2: # end lane
+                endLanes.append(line)
+            elif line[0] > 120:
+                rightLanes.append(line)
+
+        for right in rightLanes:
+            if len(rightLane) == 0:
+                rightLane = right
+            if getIntercept(right) < getIntercept(rightLane):
+                rightLane = right
+        for left in leftLanes:
+            if len(leftLane) == 0:
+                leftLane = left
+            if getIntercept(left) > getIntercept(leftLane):
+                leftLane = left
+    except:
+        pass
+        
+    return leftLane, rightLane, endLanes
+
+def getMidPositionOfX(lines, width):
+    sum = 0
+    for line in lines:
+        sum += (line[0] + line[2])/2
+    sum /= len(lines)
+    if sum < width/2:
+        return False # means left
+    else: return True # means right
+
+def getCenterPoint(leftLane, rightLane):
+    left_a = getLean(leftLane)
+    left_b = leftLane[3] - left_a*leftLane[2]
+    right_a = getLean(rightLane)
+    right_b = rightLane[1] - right_a*rightLane[0]
+
+    left_x = (30-left_b)/left_a
+    right_x = (30-right_b)/right_a
+
+    return left_x, right_x
---
+++ mecha_drive.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+import math
+
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from std_msgs.msg import Float32
+from std_msgs.msg import UInt16
+
+from function import *
+import numpy as np
+
+#This part is for the camera calibration
+mtx = np.array([[254.99142544,   0.,         311.43317687],
+ [  0.,         253.087548,   254.01836666],
+ [  0.,           0.,           1.,        ]])
+
+dist = np.array([[-0.24186327,  0.03653006, -0.00916361, -0.00039632, -0.00048728]])
+
+
+prev_command = 0
+
+
+
+class core_processing:
+
+    def __init__(self):
+        self.bridge = CvBridge()
+
+        self.image_sub = rospy.Subscriber("image_raw",Image,self.callback) #original image subscriber
+        self.stepper = rospy.Publisher('stepper_control',Int32,queue_size=10)
+        self.speed = rospy.Publisher('spd',Int32,queue_size=10)
+        self.arm = rospy.Publisher('servo_control',Int32,queue_size=10)
+
+    def callback(self,data):
+
+        global prev_command
+        try:    
+            #image image to calibrated
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
+            cv_image = cv2.resize(cv_image,dsize=(640,480),interpolation=cv2.INTER_AREA)
+
+            h,  w = cv_image.shape[:2]
+            newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+            dst = cv2.undistort(cv_image, mtx, dist, None, newcameramtx)
+            x,y,w,h = roi
+            dst = dst[y:y+h, x:x+w]
+            cv_image = dst
+
+            #set the region of interest
+            ROI_image = cv_image[200:400, :]
+
+            #set ROI image to image that will be processed
+            cv_image = ROI_image
+
+            #Get the height and width of the image
+            height, width = cv_image.shape[:2]
+
+            #Make image to gray scale
+            gray_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+            #Gaussian blur to eliminate noise of the image
+            blur_img = gaussian_blur(gray_img, 5)
+
+            #Canny conversion to get the line in the image
+            canny_img = canny(blur_img, 100, 200)
+
+            vertices = np.array([[(50,height),(width/2-45, height/2+60), (width/2+45, height/2+60), (width-50,height)]], dtype=np.int32)
+
+            #get hough lines
+            hough_img, lines = hough_lines(canny_img, 1, 1 * np.pi/180, 55, 10, 5)
+
+            #Make grayscale image to BGR image
+            canny_img = cv2.cvtColor(canny_img, cv2.COLOR_GRAY2BGR)
+
+            #get the contact points
+            points = getContactPoints(canny_img)
+
+            #draw points
+            canny_img = drawContactPoints(canny_img, points)
+
+            #draw crossline
+            canny_img = draw_crossLines(canny_img)
+
+            #integrate two image(hough,canny)
+            hough_img = weighted_img(hough_img, canny_img)
+
+            H1LD = points['H1LD']
+            H1RD = points['H1RD']
+            H2LD = points['H2LD']
+            H2RD = points['H2RD']
+            H3LD = points['H3LD']
+            H3RD = points['H3RD']
+            H1Y = int(height/3)*1
+            H2Y = int(height/3)*2
+            H3Y = int(height/3)*3
+
+            V1D = points['V1D']
+            V2D = points['V2D']
+            V3D = points['V3D']
+            V4D = points['V4D']
+            V5D = points['V5D']
+            V6D = points['V6D']
+            V7D = points['V7D']
+            V1X = int(width/8)*1        # 1 block distance by y axis
+            V2X = int(width/8)*2        # 2 blocks distance by y axis
+            V3X = int(width/8)*3        # 3 blocks distance by y axis
+            V4X = int(width/8)*4        # 4 blocks distance by y axis
+            V5X = int(width/8)*5        # 5 blocks distance by y axis
+            V6X = int(width/8)*6        # 6 blocks distance by y axis
+            V7X = int(width/8)*7        # 7 blocks distance by y axis
+
+            XD1 = int(width/8)*1        # 1 block distance by x axis
+            XD2 = int(width/8)*2        # 2 blocks distance by x axis
+            XD3 = int(width/8)*3        # 3 blocks distance by x axis
+
+            STOP = 12
+            STRAIGHT = 0
+            TO_3 = 3
+            BACKWARD = 6
+            TO_9 = 9
+            CLOCKWISE = 33
+            COUNTER_CLOCKWISE = 99
+            RIGHT_1 = 110
+            RIGHT_2 = 120
+            RIGHT_3 = 130
+            LEFT_1 = 210
+            LEFT_2 = 220
+            LEFT_3 = 230
+            
+            leftLane, rightLane, endLane = getLane(lines) # determine the existance of the line(left,right,end)
+            left = right = end = False
+            if len(leftLane) != 0:
+                left = True
+            if len(rightLane) != 0:
+                right = True
+            if len(endLane) != 0:
+                end = True
+            
+            #print(left,right,end)
+            #print(V1D,V2D,V3D,V4D,V5D,V6D,V7D)
+            ##############################
+
+            ############################### algorithm ######################################################################
+            #if(left and end and V3D > 165):
+            minusSumIncline = (H1LD+H2LD+H3LD)/3 - (H1RD+H2RD+H3RD)/3
+
+            '''(H1LD+H2LD+H3LD)/2
+            if left and end and not right:
+                speed_command = RIGHT_3
+            if
+            '''
+            speed_command = STRAIGHT
+            if minusSumIncline < -20:
+                speed_command = RIGHT_1
+            elif minusSumIncline > 20:
+                speed_command = LEFT_1
+            if not left and minusSumIncline 
+            '''
+            print("=======================")
+            print((H3RD + H2RD + H1RD)/3)
+            print((H3LD + H2LD + H1LD)/3)'''
+            #print('\n'.join[f'{i},{j}' for i,j in points.items()])
+            '''if(left and end and V3D > 165):
+                speed_command = STRAIGHT
+                print('case1')
+            elif(left and end and right and  V3D > 165):
+                speed_command = STOP
+                print('case2')
+
+            elif(left and end and right and  V3D < 165 or H2RD > 220):
+                speed_command = COUNTER_CLOCKWISE
+                print('case3')
+
+            elif(left and end and not right and V3D < 165):
+                speed_command = TO_9
+                print('case4')
+            else:
+                speed_command = prev_command
+                print('prev')'''
+            ############################### algorithm ######################################################################
+
+            
+            
+            self.stepper.publish(speed_command)
+
+            #speed could be 122,490,3921
+            #self.speed.publish(490)
+
+            cv2.imshow('hough',hough_img)
+            cv2.imshow('cv',cv_image)
+            #cv2.imshow('ROI',ROI_image)
+
+            cv2.waitKey(1)
+
+
+
+        except CvBridgeError as e:
+            print(e)
+
+def main(args):
+    cp = core_processing()
+    rospy.init_node('core', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+   
+if __name__ == '__main__':
+    main(sys.argv)
+
